f4847b90fd4111c8d0c25163ff0682f552d7fd8e
Samuel Dindyal <sam.dindyal@me.com>
2016-11-13 14:14:03 -0500

    Security fixes and refactoring

--- cgi-bin/lab6.cgi
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/ruby
-
-puts "Content-type: text/html\n\n"
-puts "HTTP/1.0 200 OK"
-puts "<html><body>This is a test</body></html>"
--- cgi-bin/lab6.rb
+++ cgi-bin/lab6.rb
@@ -5,18 +5,20 @@ require 'cgi'
 puts "Content-type: text/html\n\n"
 
 cgi = CGI.new
-fname = cgi['fname']
-lname = cgi['lname']
-address = cgi['address'].split.map(&:capitalize).join(' ')
-phone = cgi['phone']
+fname = CGI.escape cgi['fname']
+lname = CGI.escape cgi['lname']
+address = CGI.escape cgi['address'].split.map(&:capitalize).join(' ')
+phone = CGI.escape cgi['phone']
 
-puts "<div id=\"content\"><h1>Thank you for your submission, #{fname} !</h1>"
-puts "<h3>Your information is as follows:</h3><br>"
-puts "<ul>"
-puts "<li class=\"list-item\">First Name: #{fname}</li>"
-puts "<li class=\"list-item\">Last Name: #{lname}</li>"
-puts "<li class=\"list-item\">Address: #{address}</li>"
-puts "<li class=\"list-item\">Phone:"
+puts <<-HTML
+  <div id="content"><h1>Thank you for your submission, #{fname} !</h1>
+    <h3>Your information is as follows:</h3><br>
+    <ul>
+        <li class="list-item">First Name: #{fname}</li>
+        <li class="list-item">Last Name: #{lname}</li>
+        <li class="list-item">Address: #{address}</li>
+        <li class="list-item">Phone:
+HTML
 
 phone.split('-').each_with_index do |set, index|
   if index == 0
@@ -30,6 +32,8 @@ phone.split('-').each_with_index do |set, index|
   puts "</span>"
 end
 
-puts "</li>"
-puts "</ul>"
-puts "</div>"
+puts <<-HTML
+    </li>
+  </ul>
+</div>
+HTML
